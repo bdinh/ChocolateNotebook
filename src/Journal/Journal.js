846ee1021833f3869ea914cf203cf89c57649606
aamiller <adelem@uw.edu>
2017-11-30 19:58:06 -0800

    More journal formatting, broke up components, almost finished search bar and new entry line

--- src/Journal/Journal.js
+++ src/Journal/Journal.js
@@ -6,36 +6,95 @@ import { Input, InputGroup,} from 'reactstrap';
 class Journal extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
+      <div className="entry-search-row" >
+      <NewEntryButton />
+      <SearchJournal />
+      </div>
       <JournalEntryItem />
       </div>
     );
   }
 }
 
+// Renders card header
+class NewEntryButton extends Component {
+  render() {
+    return( 
+      <button id="entry-button">New Entry</button>
+    );
+  }
+}
+
+class SearchJournal extends Component {
+  render() {
+    return (
+      <span className="search-elements"> 
+      <i className="fa fa-search" aria-hidden="true"></i>
+      <p>Search Journal</p>
+      </span>
+    );
+  }
+}
+
+
 // Displays existing journal entry items
 class JournalEntryItem extends Component {
   render() {
     return (
       <div className="journal-item">
-      <div className="journal-entry-header">
-      <div className="header-field">Kokoa Kamili 70% Dark Chocolate</div>
-      <div className="header-field">11/18/2017</div>
-      </div>
+      <JournalCardHeader date={"11/18/2017"} barName={"Kokoa Kamili 70% Dark Chocolate"} />
       
       <div className="journal-entry-main">
-      <div className="chocolate-detail">
-      <i className="fa fa-globe" aria-label="Origin"></i><p> Africa </p>
-      <i className="fa fa-industry" aria-label="Producer"></i><p>Producer </p>
-      <i className="fa fa-sticky-note" aria-label="Tasting notes"></i>
-      <p>Floral, Roasted, Butter</p>
-      </div>
-      <p>Rating: X X X X X</p>
-      <div className="chocolate-rating-text-container">
-      <p>Tastes delicious! Best chocolate bar I've ever had!</p>
-      </div>
-      </div>
+      <div className="chocolate-detail-container">
+      <ChocolateDetailsStatic region={"Africa"} producer={"Kokoa Kamili"} tastingNotes={"Floral, Roasted, Buttery"} />
       
+      <p>Rating: <i className="fa fa-star" aria-hidden="true"></i> <i className="fa fa-star-o" aria-hidden="true"></i> <i className="fa fa-star-o" aria-hidden="true"></i> <i className="fa fa-star-o" aria-hidden="true"></i> <i className="fa fa-star-o" aria-hidden="true"></i></p>
+      </div>
+      <JournalTextHolder text={"Tastes delicious! Best chocolate bar I've ever had!"} isNewEntry={false} />
+      </div>
+      </div>
+    );
+  }
+}
+
+// Renders card header
+class JournalCardHeader extends Component {
+  render() {
+    return( 
+      <div className="journal-entry-header">
+      <div className="header-field">{this.props.barName}</div>
+      <div className="header-field">{this.props.date}</div>
+      </div>
+    );
+  }
+}
+
+// Renders text box on card
+// Takes a boolean prop, isNewEntry, if 
+// this is being rendered as part of a new entry and not
+// an existing one.
+class JournalTextHolder extends Component {
+  render() {
+    return ( 
+      <div className="chocolate-rating-text-container">
+      <p>{this.props.text}</p>
+      </div>
+    );
+  }
+}
+
+// Statically renders details about the chocolate
+// Props: region, producer, tastingNotes
+class ChocolateDetailsStatic extends Component {
+  render() {
+    return ( 
+      <div className="chocolate-detail">
+      <i className="fa fa-globe" aria-label="Origin"></i><p>{this.props.region}</p>
+      <i className="fa fa-industry" aria-label="Producer"></i><p>{this.props.producer}
+      </p>
+      <i className="fa fa-sticky-note" aria-label="Tasting notes"></i>
+      <p>{this.props.tastingNotes}</p>
       </div>
     );
   }
