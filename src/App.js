c4b482f56dd8f3df900745b54953bcdb795f5b30
bdinh <baodinh96@gmail.com>
2017-12-05 19:18:06 -0800

    styled login page

--- src/App.js
+++ src/App.js
@@ -13,146 +13,146 @@ import firebase from 'firebase/app';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      loading : true,
-      errorMessage : null,
-      userData:  {
-        userName : null,
-        userJournalEntries : null,
-        plan : null
-      }
-    };
-  }
-
-  componentDidMount() {
-    this.authUnRegFunc = firebase.auth().onAuthStateChanged((firebaseUser) => {
-      if (firebaseUser){ // User logged in
-        this.setState({user: firebaseUser});
-        this.userDataRef = firebase.database().ref('userData/' + firebaseUser.uid)
-        this.userDataRef.on('value', (snapshot) => {
-          this.setState({userData: snapshot.val(), loading: false});
-        });
-      } else { // User logged out
-        this.setState({user:null, loading: false});
-      }
-    });
-  }
-
-  componentWillUnmount() {
-    this.authUnRegFunc();
-    this.userDataRef.off();
-  }
-
-  //signs up a new user.
-  handleSignUp(email, password)  {
-    this.setState({errorMessage:null});
-    this.setState({loading:true});
-    let promise = firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then((firebaseUser) =>  {
-      firebaseUser.updateProfile({
-        displayName: email
-      });
-
-      // Add to database a store for the user
-      let newUserData = {
-        userName : email,
-        userJournalEntries : "None",
-        plan : null
-      }
-
-      let userDataRef = firebase.database().ref('userData/' + firebaseUser.uid)
-      .set(newUserData);
-      // userDataRef.child(firebaseUser.uid).push(newUserData);
-
-    })
-    .catch((error) =>  {
-      this.setState({errorMessage : error.message});
-      this.setState({loading:false});
-    });
-  }
-
-  //logs an existing user in.
-  handleSignIn(email, password) {
-    this.setState({errorMessage:null}); //clear old error
-    this.setState({loading:true});
-    firebase.auth().signInWithEmailAndPassword(email, password)
-    .catch((error) => {
-      this.setState({errorMessage: error.message});
-      this.setState({loading:false});
-    });
-  }
-
-  handleSignOut() {
-    this.setState({loading: true, errorMessage:null});
-    firebase.auth().signOut()
-    .catch((err) =>   {
-      this.setState({loading: false, errorMessage: err.message});
-    });
-  }
-
-  handleAddSubscription(plan)  {
-    let setPlan = firebase.database().ref('userData/' + this.state.user.uid + '/plan')
-    .set(plan);
-  }
-
-  handleUnsubscribe()  {
-    let setNull = firebase.database().ref('userData/' + this.state.user.uid + '/plan')
-    .set(null);
-  }
-
-  render() {
-    if (this.state.loading) { // If loading, display spinner
-      return (
-        <div className="loading-screen">
-        <i className="fa fa-spinner fa-spin fa-3x" aria-label="Connecting..."></i>
-        </div>
-      )
-    } else {
-    let contents = null;
-    if (!this.state.user) {
-      contents = (
-        <div>
-        <Switch>
-          <Route exact path="/" component={(props) => <LandingPage/>}/>
-          <Route exact path="/login" component={(props) => <Login signInCallback={(e,p) =>
-              this.handleSignIn(e,p)}/>}/>
-          <Route exact path="/signup" component={(props) => <SignUp signUpCallback={(e,p) =>
-                this.handleSignUp(e,p)}/>}/>
-          <Redirect to="/"/>
-        </Switch>
-        <p>{this.state.errorMessage}</p>
-        </div>);
-        } else {
-          contents = (
-            <div className='logged'>
-            {/* <Nav user={this.state.user} handleSignOutCallback={() => this.handleSignOut()}/> */}
-            <Switch>
-              <Route exact path="/catalog" component={(props) => <Catalog />}></Route>
-              <Route exact path="/journal" component={(props) => <Journal currentUser={this.state.user} />}></Route>
-              <Route exact path="/subscription" component={(props) => <Subscription
-              loading={this.state.loading} subscription={this.state.userData.plan} routerprops={props}
-              user={this.state.user} handleUnsubscribe={() => this.handleUnsubscribe()}/>}></Route>
-              {this.state.userData && !this.state.userData.plan ?
-              <Route exact path="/subscribe/:plan" component={(props) =>
-              <Subscribe routerprops={props} handleAddSubscription={(plan) =>
-                this.handleAddSubscription(plan)}/>}></Route> : ''}
-              <Route exact path="/newjournalentry" component={() => <JournalNewEntry
-                currentUser={this.state.user} />}></Route>
-              <Redirect to="/journal"></Redirect>
-            </Switch>
-            </div>
-          );
-        }
-        return (
-          <div>
-            <Nav user={this.state.user} handleSignOutCallback={() => this.handleSignOut()}/>
-            {contents}
-          </div>
-        );
-      }
+    constructor(props){
+        super(props);
+        this.state = {
+            loading : true,
+            errorMessage : null,
+            userData:  {
+                userName : null,
+                userJournalEntries : null,
+                plan : null
+            }
+        };
     }
-  }
+
+    componentDidMount() {
+        this.authUnRegFunc = firebase.auth().onAuthStateChanged((firebaseUser) => {
+            if (firebaseUser){ // User logged in
+                this.setState({user: firebaseUser});
+                this.userDataRef = firebase.database().ref('userData/' + firebaseUser.uid)
+                this.userDataRef.on('value', (snapshot) => {
+                    this.setState({userData: snapshot.val(), loading: false});
+                });
+            } else { // User logged out
+                this.setState({user:null, loading: false});
+            }
+        });
+    }
+
+    componentWillUnmount() {
+        this.authUnRegFunc();
+        this.userDataRef.off();
+    }
+
+    //signs up a new user.
+    handleSignUp(email, password)  {
+        this.setState({errorMessage:null});
+        this.setState({loading:true});
+        let promise = firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then((firebaseUser) =>  {
+                firebaseUser.updateProfile({
+                    displayName: email
+                });
+
+                // Add to database a store for the user
+                let newUserData = {
+                    userName : email,
+                    userJournalEntries : "None",
+                    plan : null
+                }
+
+                let userDataRef = firebase.database().ref('userData/' + firebaseUser.uid)
+                    .set(newUserData);
+                // userDataRef.child(firebaseUser.uid).push(newUserData);
+
+            })
+            .catch((error) =>  {
+                this.setState({errorMessage : error.message});
+                this.setState({loading:false});
+            });
+    }
+
+    //logs an existing user in.
+    handleSignIn(email, password) {
+        this.setState({errorMessage:null}); //clear old error
+        this.setState({loading:true});
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .catch((error) => {
+                this.setState({errorMessage: error.message});
+                this.setState({loading:false});
+            });
+    }
+
+    handleSignOut() {
+        this.setState({loading: true, errorMessage:null});
+        firebase.auth().signOut()
+            .catch((err) =>   {
+                this.setState({loading: false, errorMessage: err.message});
+            });
+    }
+
+    handleAddSubscription(plan)  {
+        let setPlan = firebase.database().ref('userData/' + this.state.user.uid + '/plan')
+            .set(plan);
+    }
+
+    handleUnsubscribe()  {
+        let setNull = firebase.database().ref('userData/' + this.state.user.uid + '/plan')
+            .set(null);
+    }
+
+    render() {
+        if (this.state.loading) { // If loading, display spinner
+            return (
+                <div className="loading-screen">
+                  <i className="fa fa-spinner fa-spin fa-3x" aria-label="Connecting..."></i>
+                </div>
+            )
+        } else {
+            let contents = null;
+            if (!this.state.user) {
+                contents = (
+                    <div>
+                      <Switch>
+                        <Route exact path="/" component={(props) => <LandingPage/>}/>
+                        <Route exact path="/login" component={(props) => <Login signInCallback={(e,p) =>
+                            this.handleSignIn(e,p)}/>}/>
+                        <Route exact path="/signup" component={(props) => <SignUp signUpCallback={(e,p) =>
+                            this.handleSignUp(e,p)}/>}/>
+                        <Redirect to="/"/>
+                      </Switch>
+                      <p>{this.state.errorMessage}</p>
+                    </div>);
+            } else {
+                contents = (
+                    <div className='logged'>
+                        {/* <Nav user={this.state.user} handleSignOutCallback={() => this.handleSignOut()}/> */}
+                      <Switch>
+                        <Route exact path="/catalog" component={(props) => <Catalog />}></Route>
+                        <Route exact path="/journal" component={(props) => <Journal currentUser={this.state.user} />}></Route>
+                        <Route exact path="/subscription" component={(props) => <Subscription
+                            loading={this.state.loading} subscription={this.state.userData.plan} routerprops={props}
+                            user={this.state.user} handleUnsubscribe={() => this.handleUnsubscribe()}/>}></Route>
+                          {this.state.userData && !this.state.userData.plan ?
+                              <Route exact path="/subscribe/:plan" component={(props) =>
+                                  <Subscribe routerprops={props} handleAddSubscription={(plan) =>
+                                      this.handleAddSubscription(plan)}/>}></Route> : ''}
+                        <Route exact path="/newjournalentry" component={() => <JournalNewEntry
+                            currentUser={this.state.user} />}></Route>
+                        <Redirect to="/journal"></Redirect>
+                      </Switch>
+                    </div>
+                );
+            }
+            return (
+                <div>
+                  <Nav user={this.state.user} handleSignOutCallback={() => this.handleSignOut()}/>
+                    {contents}
+                </div>
+            );
+        }
+    }
+}
 
 export default App;
--- src/Users/Login.js
+++ src/Users/Login.js
@@ -4,77 +4,107 @@ import { Link  } from 'react-router-dom';
 import './Auth.css';
 
 class Login extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      email: undefined,
-      password: undefined
-    }; //initialize state
+    constructor(props){
+        super(props);
+        this.state = {
+            email: undefined,
+            password: undefined
+        }; //initialize state
 
-  }
-
-  //handle signIn Button
-  handleSignIn(event) {
-    event.preventDefault(); //don't submit
-    this.props.signInCallback(this.state.email, this.state.password);
-
-  }
-
-  handleChange(event) {
-    let newState = this.state;
-    newState[event.target.name] = event.target.value;
-    this.setState(newState);
-  }
-
-  /* SignUpForm#render() */
-  render() {
-    if (this.state.loading) {
-      return (
-        <div className="text-center">
-        <i className="fa fa-spinner fa-spin fa-3x" aria-label="Connecting..."></i>
-        </div>
-      )
-    } else {
-      return (
-        <div id="login">
-        {/* email */}
-        <form>
-        <label>Email</label>
-          <input id="email"
-          type="email"
-          name="email"
-          onChange={((event) => {this.handleChange(event)}) }
-          valid={this.emailValid}
-          />
-          {this.emailErrors}
-        </form>
-
-        {/* password */}
-        <form>
-        <label>Password</label>
-        <input id="password"
-        type="password"
-        name="password"
-        onChange={((event) => {this.handleChange(event)}) }
-        valid={this.passwordValid}
-        />
-        {this.passwordErrors}
-      </form>
-
-        {/* Buttons */}
-        <form>
-        <button className="mr-2" color="primary" onClick={(e) => this.handleSignIn(e)}>
-          Sign-in
-        </button>
-        <button color="primary">
-          <Link to='/signup'>Need to make an account? Sign Up Here</Link>
-
-        </button>
-      </form>
-    </div>
-      )
     }
-  }
+
+    //handle signIn Button
+    handleSignIn(event) {
+        event.preventDefault(); //don't submit
+        this.props.signInCallback(this.state.email, this.state.password);
+
+    }
+
+    handleChange(event) {
+        let newState = this.state;
+        newState[event.target.name] = event.target.value;
+        this.setState(newState);
+    }
+
+    /* SignUpForm#render() */
+    render() {
+        if (this.state.loading) {
+            return (
+                <div className="text-center">
+                  <i className="fa fa-spinner fa-spin fa-3x" aria-label="Connecting..."></i>
+                </div>
+            )
+        } else {
+            return (
+                <div id="login">
+                    {/* email */}
+                  <form>
+                    <label>Email</label>
+                    <input id="email"
+                           type="email"
+                           name="email"
+                           onChange={((event) => {this.handleChange(event)}) }
+                           valid={this.emailValid}
+                    />
+                      {this.emailErrors}
+                  </form>
+
+                    {/* password */}
+                  <form>
+                    <label>Password</label>
+                    <input id="password"
+                           type="password"
+                           name="password"
+                           onChange={((event) => {this.handleChange(event)}) }
+                           valid={this.passwordValid}
+                    />
+                      {this.passwordErrors}
+                  </form>
+
+                    {/* Buttons */}
+                  <form>
+                    <button className="mr-2" color="primary" onClick={(e) => this.handleSignIn(e)}>
+                      Sign-in
+                    </button>
+                    <button color="primary">
+                      <Link to='/signup'>Need to make an account? Sign Up Here</Link>
+
+                    </button>
+                  </form>
+                </div>
+            )
+        }
+    }
 }
 
-  export default Login; //the default
+export default Login; //the default
+
+          {/*<div className="row row-container">*/}
+            {/*<div className="login-banner-container">*/}
+            {/*</div>*/}
+            {/*<div className="user-login-container">*/}
+              {/*<div className="login-form">*/}
+                {/*<div className="form-group email-form">*/}
+                  {/*<label  className="form-labels" htmlFor="loginEmail">Email address</label>*/}
+                  {/*<input*/}
+                      {/*type="email"*/}
+                      {/*className="form-control"*/}
+                      {/*id="loginEmail"*/}
+                      {/*aria-describedby="emailHelp"*/}
+                      {/*placeholder="Enter email"*/}
+                  {/*/>*/}
+                {/*</div>*/}
+                {/*<div className="form-group form-group-spacing">*/}
+                  {/*<label className="form-labels" htmlFor="loginPassword">Password</label>*/}
+                  {/*<input*/}
+                      {/*type="password"*/}
+                      {/*className="form-control"*/}
+                      {/*id="loginPassword"*/}
+                      {/*placeholder="Password"/>*/}
+                {/*</div>*/}
+                {/*<div className="new-account-text-container">*/}
+                  {/*<p className="new-account-text">Don't have an account? <Link to='./join'>Join</Link></p>*/}
+                {/*</div>*/}
+            {/*</div>*/}
+            {/*</div>*/}
+
