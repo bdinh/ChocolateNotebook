f8f7ced8b00cddf0adb3f6ae91c83e92fc0b8ccd
mckaulay <mckaulay@gmail.com>
2017-12-06 11:24:27 -0800

    basic catalog margins

--- src/Catalog/Catalog.js
+++ src/Catalog/Catalog.js
@@ -6,7 +6,7 @@ import "./Catalog.css";
 export class Catalog extends Component {
     render() {
         return (
-            <div>
+            <div className="catalog-container">
                 <SearchCatalog />
                 <div className="row">
                     <div className="col-m-4">
