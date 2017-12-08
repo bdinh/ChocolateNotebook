a3a0f3bdf4372a071d2378f4aed0fc2ef8cd8f78
jchuang02 <jchuangsa@gmail.com>
2017-12-07 17:03:17 -0800

    responsiveness for subscription will be added

--- src/Subscription/Subscription.js
+++ src/Subscription/Subscription.js
@@ -19,14 +19,20 @@ import SubscriptionInfo from './SubscriptionInfo';
             <header>
               <h1>Get ChocoBox Today!</h1>
               <div className="description">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur
-                  adipisicing elit. Deleniti porro odit voluptas vitae cupiditate
-                  excepturi eveniet voluptatem, culpa ratione id veritatis itaque
-                  voluptatum modi unde cum inventore aliquam quisquam tenetur?
-                </p>
-                <Link to="/catalog"><button>View Our Catalogue</button></Link>
-                <a href="#learn"><button href="learn">Learn More</button></a>
+                <div>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Deleniti porro odit voluptas vitae cupiditate
+                  </p>
+                </div>
+                <div className="sub-links">
+                  <div>
+                    <Link to="/catalog"><button>View Our Catalogue</button></Link>
+                  </div>
+                  <div>
+                    <a href="#learn"><button href="learn">Learn More</button></a>
+                  </div>
+                </div>
               </div>
             </header>
             <div className="cards">
@@ -51,7 +57,9 @@ import SubscriptionInfo from './SubscriptionInfo';
             </div>
             <div id="learn" className="learn-more">
               <div className="learn-card">
-                <img className="learn-image" src="/choices.svg" alt="choose-plan"/>
+                <div>
+                  <img className="learn-image" src="/choices.svg" alt="choose-plan"/>
+                </div>
                 {/* <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> */}
                 <div>
                   <h3>Choose A Plan</h3>
@@ -64,6 +72,9 @@ import SubscriptionInfo from './SubscriptionInfo';
               </div>
               <div className="learn-card">
                 <div>
+                  <img className="learn-image" src="/chocolate-icon.svg" alt="pick-chocolate"/>
+                </div>
+                <div>
                   <h3>Choose Your Chocolates</h3>
                   <p>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi illum,
@@ -71,18 +82,19 @@ import SubscriptionInfo from './SubscriptionInfo';
                      ipsum dolores aperiam et error! Dignissimos aspernatur harum totam soluta.
                   </p>
                 </div>
-                <img className="learn-image" src="/chocolate-icon.svg" alt="pick-chocolate"/>
                 {/* <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> */}
               </div>
               <div className="learn-card">
-                <img className="learn-image" src="/letter.svg" alt="get-in-mail"/>
+                <div>
+                  <img className="learn-image" src="/letter.svg" alt="get-in-mail"/>
+                </div>
 {/* <div>Icons made by <a href="https://www.flaticon.com/authors/pixel-buddha" title="Pixel Buddha">Pixel Buddha</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> */}
                 <div>
                   <h3>Wait For Your Box!</h3>
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi illum,
-                    iusto fuga harum provident, ipsa voluptates debitis quibusdam. Deleniti
-                     ipsum dolores aperiam et error! Dignissimos aspernatur harum totam soluta.
+                    We ship all around the world! Once you have placed your order, we'll
+                    send you a tracking number so that you can keep a close eye on those
+                    precious chocolates.
                   </p>
                 </div>
               </div>
